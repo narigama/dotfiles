0eee19acee2d2434e32f22cf5f7742234d68c04a
David Smith <david@narigama.dev>
2023-02-25 01:40:55 +0000

    removed: autocomplete(tmp), added: better nav

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -6,17 +6,21 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- better vertical nav
-vim.keymap.set({'n', 'v'}, '<Space>', '<Nop>', {
-    silent = true
-})
-vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", {
-    expr = true,
-    silent = true
-})
-vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", {
-    expr = true,
-    silent = true
-})
+-- better up/down
+vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+
+-- Move to window using the <ctrl> hjkl keys
+vim.keymap.set("n", "<C-h>", "<C-w>h", { silent = true })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { silent = true })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { silent = true })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { silent = true })
+
+-- Resize window using <ctrl> arrow keys
+vim.keymap.set("n", "<C-Up>", "<cmd>resize +2<cr>", { silent = true })
+vim.keymap.set("n", "<C-Down>", "<cmd>resize -2<cr>", { silent = true })
+vim.keymap.set("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { silent = true })
+vim.keymap.set("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { silent = true })
 
 -- disable netrw
 vim.g.loaded_netrw = 1
