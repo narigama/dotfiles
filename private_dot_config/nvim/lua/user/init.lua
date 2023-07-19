c9745a1a41c61af22a84675cf0334e97f93f7e7d
David Smith <david@narigama.dev>
2023-07-19 16:45:03 +0100

    Added hop, an easymotion clone written in lua

--- private_dot_config/nvim/lua/user/init.lua
+++ private_dot_config/nvim/lua/user/init.lua
@@ -11,6 +11,31 @@ return {
         }
       end,
     },
+    {
+      "phaazon/hop.nvim",
+      branch = "v2",
+      lazy = false,
+      config = function()
+        -- setup and config colours
+        require("hop").setup {
+          case_insensitive = true,
+
+        }
+        vim.cmd("hi HopNextKey guifg=#00ff00")
+        vim.cmd("hi HopNextKey1 guifg=#00ff00")
+        vim.cmd("hi HopNextKey2 guifg=#00a300")
+
+        -- bind to easymotion-like keys
+        vim.api.nvim_set_keymap("n", "<Leader><Leader>b", "<cmd>HopWordBC<CR>", {noremap=true})
+        vim.api.nvim_set_keymap("n", "<Leader><Leader>w", "<cmd>HopWordAC<CR>", {noremap=true})
+        vim.api.nvim_set_keymap("n", "<Leader><Leader>j", "<cmd>HopLineAC<CR>", {noremap=true})
+        vim.api.nvim_set_keymap("n", "<Leader><Leader>k", "<cmd>HopLineBC<CR>", {noremap=true})
+        vim.api.nvim_set_keymap("v", "<Leader><Leader>w", "<cmd>HopWordAC<CR>", {noremap=true})
+        vim.api.nvim_set_keymap("v", "<Leader><Leader>b", "<cmd>HopWordBC<CR>", {noremap=true})
+        vim.api.nvim_set_keymap("v", "<Leader><Leader>j", "<cmd>HopLineAC<CR>", {noremap=true})
+        vim.api.nvim_set_keymap("v", "<Leader><Leader>k", "<cmd>HopLineBC<CR>", {noremap=true})
+      end,
+    }
   },
 
   lsp = {
