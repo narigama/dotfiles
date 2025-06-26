43e65059e666b63d4cba66a9b1d7fd49cd7611f8
David Smith <david@narigama.dev>
2025-06-26 17:23:29 +0100

    misc updates

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -1,15 +1,18 @@
 -- bootstrap lazy.nvim, then use it to bootstrap astronvim
-local lazypath = vim.env.LAZY or vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+local lazypath = vim.env.LAZY or vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.env.LAZY or (vim.uv or vim.loop).fs_stat(lazypath)) then
   -- stylua: ignore
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable",
+    lazypath })
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- validate that lazy is available
 if not pcall(require, "lazy") then
   -- stylua: ignore
-  vim.api.nvim_echo({ { ("Unable to load lazy from: %s\n"):format(lazypath), "ErrorMsg" }, { "Press any key to exit...", "MoreMsg" } }, true, {})
+  vim.api.nvim_echo(
+  { { ("Unable to load lazy from: %s\n"):format(lazypath), "ErrorMsg" }, { "Press any key to exit...", "MoreMsg" } },
+    true, {})
   vim.fn.getchar()
   vim.cmd.quit()
 end
@@ -20,8 +23,8 @@ require("lazy").setup({
     "AstroNvim/AstroNvim",
     version = "^4",
     import = "astronvim.plugins",
-    opts = { -- AstroNvim options must be set here with the `import` key
-      mapleader = " ", -- This ensures the leader key must be configured before Lazy is set up
+    opts = {             -- AstroNvim options must be set here with the `import` key
+      mapleader = " ",   -- This ensures the leader key must be configured before Lazy is set up
       maplocalleader = ",", -- This ensures the localleader key must be configured before Lazy is set up
       icons_enabled = true, -- Set to false to disable icons (if no Nerd Font is available)
       pin_plugins = nil, -- Default will pin plugins when tracking `version` of AstroNvim, set to true/false to override
@@ -76,13 +79,13 @@ require("lazy").setup({
     lazy = false,
     config = function()
       -- setup and config colours
-      require("hop").setup {
+      require("hop").setup({
         case_insensitive = true,
-      }
+      })
 
-      vim.cmd "hi HopNextKey guifg=#00ff00"
-      vim.cmd "hi HopNextKey1 guifg=#00ff00"
-      vim.cmd "hi HopNextKey2 guifg=#00a300"
+      vim.cmd("hi HopNextKey guifg=#00ff00")
+      vim.cmd("hi HopNextKey1 guifg=#00ff00")
+      vim.cmd("hi HopNextKey2 guifg=#00a300")
 
       -- bind to easymotion-like keys
       vim.api.nvim_set_keymap("n", "<Leader><Leader>b", "<cmd>HopWordBC<CR>", { noremap = true })
@@ -98,6 +101,17 @@ require("lazy").setup({
   {
     "karb94/neoscroll.nvim",
     opts = {},
+  },
+  {
+    "L3MON4D3/LuaSnip",
+    config = function(plugin, opts)
+      -- include the default astronvim config that calls the setup call
+      require("astronvim.plugins.configs.luasnip")(plugin, opts)
+      -- load snippets paths
+      require("luasnip.loaders.from_vscode").lazy_load({
+        paths = { vim.fn.stdpath("config") .. "/snippets" },
+      })
+    end,
   },
 } --[[@as LazySpec]], {
   -- Configure any other `lazy.nvim` configuration options here
@@ -117,15 +131,15 @@ require("lazy").setup({
 } --[[@as LazyConfig]])
 
 -- polish
-vim.cmd "set mouse="
+vim.cmd("set mouse=")
 vim.opt.rnu = false
 vim.opt.scrolloff = 999
 vim.opt.sidescrolloff = 999
 
 -- transparency
-vim.cmd [[
+vim.cmd([[
   highlight Normal guibg=none
   highlight NonText guibg=none
   highlight Normal ctermbg=none
   highlight NonText ctermbg=none
-]]
+]])
